bef347d1a38019233b6a3a69a9b0a4b5eadde9c8
Greg McGowan <gregmcgowan@gmail.com>
2015-07-30 12:08:43 +0100

    Fix problem introduced in the previous commit. adding extra headers was removing the mandatory ones.

--- App/src/main/java/com/drownedinsound/data/network/DisApiClient.java
+++ App/src/main/java/com/drownedinsound/data/network/DisApiClient.java
@@ -224,7 +224,7 @@ public class DisApiClient {
     public void addNewPost(Board board, String title, String content) {
         String authToken = userSessionManager.getAuthenticityToken();
 
-        Headers.Builder extraHeaders = getMandatoryDefaultHeaders();
+        Headers.Builder extraHeaders = new Headers.Builder();
         extraHeaders.add("Referer", board.getUrl());
         RequestBody requestBody = new FormEncodingBuilder().add("section_id", String.valueOf(board
                 .getSectionId()))
@@ -285,12 +285,10 @@ public class DisApiClient {
             Headers.Builder extraHeaders,
             final OkHttpAsyncResponseHandler okHttpAsyncResponseHandler){
 
-        Headers.Builder headerBuilder = getMandatoryDefaultHeaders();
-        if(extraHeaders == null) {
-            extraHeaders = new Headers.Builder();
-        }
+        Headers.Builder headerBuilder = addMandatoryHeaders(extraHeaders);
+
         Request.Builder builder = new Request.Builder().
-                headers(headerBuilder.build()).headers(extraHeaders.build());
+                headers(headerBuilder.build());
 
         if (RequestMethod.GET.equals(requestMethod)) {
              performRequest(builder.get(), tag, url, okHttpAsyncResponseHandler);
@@ -329,17 +327,24 @@ public class DisApiClient {
         return inProgressRequests.contains(tag);
     }
 
-    protected Headers.Builder getMandatoryDefaultHeaders() {
-        Headers.Builder headerBuilder = new Headers.Builder();
-        headerBuilder.add("Cache-Control", "max-age=0");
-        headerBuilder.add("User-Agent",
+    protected Headers.Builder addMandatoryHeaders(){
+        return addMandatoryHeaders(null);
+    }
+
+    protected Headers.Builder addMandatoryHeaders(Headers.Builder headers) {
+        if(headers == null) {
+            headers = new Headers.Builder();
+        }
+
+        headers.add("Cache-Control", "max-age=0");
+        headers.add("User-Agent",
                 "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11");
-        headerBuilder
+        headers
                 .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-        headerBuilder.add("Accept-Encoding", "gzip,deflate,sdch");
-        headerBuilder.add("Accept-Language", "en-US,en;q=0.8,en-GB;q=0.6");
-        headerBuilder.add("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
-        return headerBuilder;
+        headers.add("Accept-Encoding", "gzip,deflate,sdch");
+        headers.add("Accept-Language", "en-US,en;q=0.8,en-GB;q=0.6");
+        headers.add("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
+        return headers;
     }
 }
 
