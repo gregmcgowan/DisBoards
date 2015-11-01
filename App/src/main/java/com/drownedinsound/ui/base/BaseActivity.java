e534c77928a055e8ce4adf8034efa342dd25aa6d
Greg McGowan <gregmcgowan@gmail.com>
2015-11-01 15:33:14 +0000

    Don't need a reference to the API client in the base activity

--- App/src/main/java/com/drownedinsound/ui/base/BaseActivity.java
+++ App/src/main/java/com/drownedinsound/ui/base/BaseActivity.java
@@ -2,14 +2,12 @@ package com.drownedinsound.ui.base;
 
 import com.drownedinsound.R;
 import com.drownedinsound.core.DisBoardsApp;
-import com.drownedinsound.data.network.DisApiClient;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-;
 
 /**
  * Created by gregmcgowan on 19/10/2014.
@@ -19,9 +17,6 @@ public abstract class BaseActivity extends AppCompatActivity implements Ui {
     protected Toolbar toolbar;
 
     protected FragmentManager fragmentManager;
-
-
-    protected DisApiClient disApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
