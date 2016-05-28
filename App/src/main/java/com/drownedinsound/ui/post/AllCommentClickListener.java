837a9bf40d290701020fe0a4acb9000913a8fb00
Greg McGowan <gregmcgowan@gmail.com>
2016-05-28 14:48:59 +0100

    Update listener for new animation

--- App/src/main/java/com/drownedinsound/ui/post/AllCommentClickListener.java
+++ App/src/main/java/com/drownedinsound/ui/post/AllCommentClickListener.java
@@ -3,10 +3,15 @@ package com.drownedinsound.ui.post;
 import com.drownedinsound.data.generatered.BoardPostComment;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.os.Build;
+import android.support.v7.widget.RecyclerView;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 /**
@@ -14,54 +19,101 @@ import android.widget.RelativeLayout;
  */
 public class AllCommentClickListener  implements View.OnClickListener{
 
-    private RelativeLayout actionLayout;
+    private ViewGroup itemViewGroup;
+
+    private TextView thisTextView;
+
+    private TextView replyTextView;
 
     private BoardPostComment boardPostComment;
 
 
     public AllCommentClickListener(
-            RelativeLayout actionLayout,
+            ViewGroup itemViewGroup,
+            TextView thisTextView,
+            TextView replyTextView,
             BoardPostComment boardPostComment) {
-        this.actionLayout = actionLayout;
+        this.replyTextView = replyTextView;
+        this.thisTextView = thisTextView;
+        this.itemViewGroup = itemViewGroup;
         this.boardPostComment = boardPostComment;
     }
 
     @Override
     public void onClick(View v) {
-        final boolean setVisible = actionLayout.getVisibility() != View.VISIBLE;
+        final boolean setVisible = thisTextView.getVisibility() != View.VISIBLE;
 
         float[] offset = setVisible ? new float[]{0, 0.5f, 1}
                 : new float[]{1, 0.5f, 0};
 
-        actionLayout.setVisibility(View.VISIBLE);
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            TransitionManager.beginDelayedTransition((RecyclerView) itemViewGroup.getParent());
+            if(setVisible) {
+                replyTextView.setVisibility(View.VISIBLE);
+                thisTextView.setVisibility(View.VISIBLE);
+            } else {
+                replyTextView.setVisibility(View.GONE);
+                thisTextView.setVisibility(View.GONE);
 
-        ObjectAnimator removeObjectAnimator = ObjectAnimator.ofFloat(
-                actionLayout, "scaleY", offset);
-        removeObjectAnimator.setDuration(500);
-        removeObjectAnimator.setInterpolator(new AccelerateInterpolator());
-        removeObjectAnimator.addListener(new Animator.AnimatorListener() {
-
-            public void onAnimationStart(Animator animation) {
             }
+        } else {
+            AnimatorSet showHideReplyAndThis = new AnimatorSet();
+            replyTextView.setVisibility(View.VISIBLE);
+            thisTextView.setVisibility(View.VISIBLE);
 
-            public void onAnimationEnd(Animator animation) {
-                if (setVisible) {
-                    actionLayout.setVisibility(View.VISIBLE);
-                } else {
-                    actionLayout.setVisibility(View.GONE);
+            ObjectAnimator thisAnimator = ObjectAnimator.ofFloat(
+                    thisTextView, "scaleY", offset);
+            thisAnimator.setDuration(500);
+            thisAnimator.setInterpolator(new AccelerateInterpolator());
+            thisAnimator.addListener(new Animator.AnimatorListener() {
+
+                public void onAnimationStart(Animator animation) {
                 }
-                boardPostComment.setActionSectionVisible(setVisible);
 
-            }
+                public void onAnimationEnd(Animator animation) {
+                    if (setVisible) {
+                        thisTextView.setVisibility(View.VISIBLE);
+                    } else {
+                        thisTextView.setVisibility(View.GONE);
+                    }
+                    boardPostComment.setActionSectionVisible(setVisible);
 
-            public void onAnimationCancel(Animator animation) {
-            }
+                }
 
-            public void onAnimationRepeat(Animator animation) {
-            }
+                public void onAnimationCancel(Animator animation) {
+                }
 
-        });
-        removeObjectAnimator.start();
+                public void onAnimationRepeat(Animator animation) {
+                }
+            });
+
+            ObjectAnimator replyAnimator = ObjectAnimator.ofFloat(
+                    replyTextView, "scaleY", offset);
+            replyAnimator.setDuration(500);
+            replyAnimator.setInterpolator(new AccelerateInterpolator());
+            replyAnimator.addListener(new Animator.AnimatorListener() {
+
+                public void onAnimationStart(Animator animation) {
+                }
+
+                public void onAnimationEnd(Animator animation) {
+                    if (setVisible) {
+                        replyTextView.setVisibility(View.VISIBLE);
+                    } else {
+                        replyTextView.setVisibility(View.GONE);
+                    }
+                    boardPostComment.setActionSectionVisible(setVisible);
+                }
+
+                public void onAnimationCancel(Animator animation) {
+                }
+
+                public void onAnimationRepeat(Animator animation) {
+                }
+            });
+            showHideReplyAndThis.playTogether(replyAnimator,thisAnimator);
+            showHideReplyAndThis.start();
+        }
     }
 
 }
